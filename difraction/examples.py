d911f7eb5673eacbc76024814da53c29c2202d24
Vladislav Nesterenko <nesterenko.vv@phystech.edu>
2022-03-20 00:09:33 +0300

    working example

--- difraction/examples.py
+++ difraction/examples.py
@@ -3,17 +3,8 @@ import numpy as np
 import math
 from scipy import integrate
 
-def f(ksi,eta,p):
-    return math.exp(ksi*ksi+(p-eta)*(p-eta))
+def f(x, y):
+    return x * y
 
-z = np.linspace(0, s, st)
-y = np.linspace(0,b,s)
-
-
-for i in range(0, s):
-    for j in range(0,st):
-        v, err = integrate.dblquad(f, 0, b, lambda eta: -100,100,args = (y[i]))
-        g[i][j] = v
-
-#plt.plot(z,y,g)
-#plt.show()
+v, err = integrate.dblquad(f, 1, 2, 1, 2)
+print(v)
