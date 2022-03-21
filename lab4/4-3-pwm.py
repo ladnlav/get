68e5835746a4ceb948f82219ae62a8935666de5a
kkriiis <m_krissstina@mail.ru>
2022-03-21 12:04:05 +0300

    4.3

---
+++ lab4/4-3-pwm.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(22, GPIO.OUT)
+
+p=GPIO.PWM(22,1000)
+
+try:
+    while True:
+        dc=input("Введите коэффициент заполнения ШИМ")
+        p.start(int(dc))
+        input("Press return to stop:")
+        p.stop()
+finally:
+    GPIO.cleanup() 
+
+
+
+GPIO.cleanup() 
